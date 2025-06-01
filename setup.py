86e75e9dde50e62b7b1f535846a2a7a3f1fcb266
Vinicio Reynoso <vr1087@gmail.com>
2025-06-01 15:17:16 -0500

    Bump version to 0.2.0 and add Conda‐release workflow

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from skbuild import setup
 
 setup(
     name="aligncount_demo",
-    version="0.1.0",
+    version="0.2.0",
     description="Aligncount wrapper",
     packages=["cli"],
     entry_points={"console_scripts": ["aligncount=cli.entrypoint:main"]},
